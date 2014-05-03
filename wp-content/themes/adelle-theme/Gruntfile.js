76e3c5d98bddf895e504856173e293f9c2c09d12
thejsj <jorge.silva@thejsj.com>
2014-05-03 12:17:32 -0400

    Adding mustache to grunt and favicons to build

--- wp-content/themes/adelle-theme/Gruntfile.js
+++ wp-content/themes/adelle-theme/Gruntfile.js
@@ -159,7 +159,7 @@ module.exports = function(grunt) {
 
 	// Tasks
 	grunt.registerTask('default', ['build','watch']);
-	grunt.registerTask('build', ['sass', 'browserify']);
-	grunt.registerTask('staging', ['build', 'uglify:dependencies', 'uglify:dev', 'favicons']);
-	grunt.registerTask('production', ['build', 'uglify', 'cssmin', 'favicons']);
+	grunt.registerTask('build', ['sass', 'browserify', 'mustache', 'favicons']);
+	grunt.registerTask('staging', ['build', 'uglify:dependencies', 'uglify:dev']);
+	grunt.registerTask('production', ['build', 'uglify', 'cssmin']);
 }
