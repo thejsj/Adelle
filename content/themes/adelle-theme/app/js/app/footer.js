19348a42bd7962ba830242df84db4a794a24bb38
thejsj <jorge.silva@thejsj.com>
2014-06-12 21:07:58 -0400

    Adding console log message for window load

--- content/themes/adelle-theme/app/js/app/footer.js
+++ content/themes/adelle-theme/app/js/app/footer.js
@@ -30,6 +30,7 @@ var Global = require('../classes/global.js');
 
 	// On Document Ready, Get All Posts through an AJAX Request
 	$(window).on('load', function(){
+		console.log('Window Load');
 		var global = new Global( MyAjax.view.posts, MyAjax.view.pages ); 
     });
 
